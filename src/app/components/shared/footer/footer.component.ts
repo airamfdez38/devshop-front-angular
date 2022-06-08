fe4a21d357b445618bec048f15c8f2efd21f68a4
maria <airamfdezs@gmail.com>
2022-06-09 00:00:17 +0200

    Footer links and styles

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faHouse, faUser, faCartShopping,  } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -11,6 +12,9 @@ export class FooterComponent implements OnInit {
   public faHouse = faHouse;
   public faUser = faUser;
   public faCartShopping = faCartShopping;
+  public faFacebook = faFacebook;
+  public faInstagram = faInstagram;
+  public faTwitter = faTwitter;
 
 
   constructor(private route: Router) { }
